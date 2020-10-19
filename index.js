be371a8a8b6db4a20fff1cadb7bd90826045d282
BatoAlvin <alvinbato112@gmail.com>
2020-10-19 08:19:40 +0300

    cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.DB_CONNECT,
 app.use(express.json());
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000/");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
